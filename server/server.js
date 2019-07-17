546091eb3dbbbd41de502ab978010378d83ef2f5
Tom Bouillut <tom@croll.fr>
2019-07-17 13:37:43 -0300

    added allowCrossDomain headers 	modifié :         server.js

--- server/server.js
+++ server/server.js
@@ -57,6 +57,15 @@ app.use(cors({
 }));
 // parse application/json
 app.use(bodyParser.json());
+//cors middleware causes express to reject connection
+// adding headers Allow-Cross-Domain
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methos', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+app.use(allowCrossDomain);
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
