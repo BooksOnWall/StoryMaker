3e1f8a16672f0d0789d0cefe807a7fd401858012
root <root@alerce2>
2019-07-17 19:09:24 +0000

    fighting with express https certificates 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -173,8 +173,8 @@ app.get('/protected', passport.authenticate('jwt', { session: false }), function
 
 // start app
 https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.cert')
 }, app)
 .listen(3010, function () {
   console.log('BooksOnWall RESTFULL Server listening on port 3010! Go to https://localhost:3000/')
