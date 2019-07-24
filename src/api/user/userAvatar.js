37a06153b44bfb868f13351e21987e77b9ec0dd8
Tom Bouillut <tom@croll.fr>
2019-07-23 21:19:23 -0300

    avatar fixes zoom border radius 	modifié :         src/api/user/userAvatar.js

--- src/api/user/userAvatar.js
+++ src/api/user/userAvatar.js
@@ -4,7 +4,11 @@ import AvatarEditor from 'react-avatar-editor';
 import { Slider } from "react-semantic-ui-range";
 
 import {
+  Form,
+  Button,
+  Checkbox,
   Input,
+  Progress,
 } from 'semantic-ui-react';
 
 import src from '../../assets/images/patrick.png';
@@ -21,19 +25,21 @@ class userAvatar extends Component {
       user: server + 'users/',
       data: null,
       authenticated: this.toggleAuthenticateStatus,
-      avatar: {
-        image: 'avatar.jpg',
-        allowZoomOut: true,
-        position: { x: 0.5, y: 0.5 },
-        scale: 1,
-        rotate: 0,
-        borderRadius: 0,
-        preview: null,
-        width: 180,
-        height: 180,
-      }
+      image: src,
+      allowZoomOut: true,
+      position: { x: 0.2, y: 0.2 },
+      scale: 1,
+      rotate: 0,
+      borderRadius: 50,
+      preview: null,
+      width: 180,
+      height: 180,
+      file: null,
+      fileName: '',
+      isUploading: false,
+      statusCode: ''
     };
-    this.handleNewImage = this.handleNewImage.bind(this);
+
     this.handleScale = this.handleScale.bind(this);
     this.handlePositionChange = this.handlePositionChange.bind(this);
     this.toggleAuthenticateStatus = this.toggleAuthenticateStatus.bind(this);
@@ -42,20 +48,25 @@ class userAvatar extends Component {
     // check authenticated status and toggle state based on that
     this.setState({ authenticated: Auth.isUserAuthenticated() })
   }
-  handleNewImage = e => {
-      this.setState({...this.state.avatar, image: e.target.files[0] })
+  handleScale = scale => {
+    try {
+      this.setState({ scale : parseFloat(scale) });
+    } catch(e) {
+      console.log(e.message);
+    }
   }
-
-  handleScale = e => {
-    console.log(e.target.value);
-      const scale = parseFloat(e.target.value);
-      this.setState({...this.state.avatar, scale : scale });
+  handleBorderRadius = borderRadius => {
+    try {
+      this.setState({ borderRadius : parseFloat(borderRadius) });
+    } catch(e) {
+      console.log(e.message);
+    }
   }
-  toggleZoomOut = e => {
-    this.setState({...this.state.avatar, allowZoomOut: true })
+  toggleZoomOut = zoomout => {
+    this.setState({ allowZoomOut: zoomout })
   }
   handlePositionChange = position => {
-      this.setState({...this.state.avatar,position: position });
+      this.setState({position: position });
   }
   onClickSave = () => {
     if (this.editor) {
@@ -64,62 +75,147 @@ class userAvatar extends Component {
       const canvas = this.editor.getImage()
 
       // If you want the image resized to the canvas size (also a HTMLCanvasElement)
-      //const canvasScaled = this.editor.getImageScaledToCanvas()
+      const canvasScaled = this.editor.getImageScaledToCanvas()
     }
   }
+  fileInputRef = React.createRef();
+
+  onFormSubmit = e => {
+    e.preventDefault(); // Stop form submit
+    //this.fileUpload(this.state.file).then(response => {
+      //console.log(response.data);
+    //});
+  };
+
+  fileChange = e => {
+    this.setState(
+      { image: e.target.files[0], file: e.target.files[0], fileName: e.target.files[0].name },
+      () => {
+        console.log(
+          "File chosen --->",
+          this.state.file,
+          console.log("File name  --->", this.state.fileName)
+        );
+      }
+    );
+  };
+
+  fileUpload = async file => {
+      const formData = new FormData();
+      formData.append("file", file);
+      try {
+        fetch.post("/file/upload/enpoint").then(response => {
+          console.log(response);
+          console.log(response.status);
+          this.setState({ statusCode: response.status }, () => {
+            console.log(
+              "This is the response status code --->",
+              this.state.statusCode
+            );
+          });
+        });
+      } catch (error) {
+        console.error(Error(`Error uploading file ${error.message}`));
+      }
+
+      const data = JSON.stringify({
+        uploadData: file
+      });
+      console.log(data);
+    };
+
+  // Export Schedules Tab 2
+  fileExport = file => {
+    // handle save for export button function
+  };
   setEditorRef = (editor) => this.editor = editor;
   render() {
+    const { statusCode } = this.state;
     return (
       <div>
         <div>
           <AvatarEditor
             ref={this.setEditorRef}
-            scale={parseFloat(this.state.avatar.scale)}
-            width={this.state.avatar.width}
-            height={this.state.avatar.height}
-            position={this.state.avatar.position}
+            scale={parseFloat(this.state.scale)}
+            width={this.state.width}
+            height={this.state.height}
+            position={this.state.position}
             onPositionChange={this.handlePositionChange}
-            rotate={parseFloat(this.state.avatar.rotate)}
-            borderRadius={this.state.avatar.width / (100 / this.state.avatar.borderRadius)}
-            image={src}
+            rotate={parseFloat(this.state.rotate)}
+            borderRadius={this.state.width / (100 / this.state.borderRadius)}
+            image={this.state.image}
             className="editor-canvas"
             />
         </div>
       <br />
-      New File:
-      <input name="newImage" type="file" onChange={this.handleNewImage} />
+        <Form onSubmit={this.onClickSave}>
+                <Form.Field>
+                  <Button
+                    content="Choose Avatar"
+                    labelPosition="left"
+                    icon="file"
+                    onClick={() => this.fileInputRef.current.click()}
+                  />
+                <input
+                    ref={this.fileInputRef}
+                    type="file"
+                    hidden
+                    onChange={this.fileChange}
+                  />
+                </Form.Field>
+                <Button  type="submit">Upload</Button>
+                  {statusCode && statusCode === "200" ? (
+                    <Progress
+                      style={{ marginTop: "20px" }}
+                      percent={100}
+                      success
+                      active
+                      progress
+                    />
+                  ) : statusCode && statusCode === "500" ? (
+                    <Progress
+                      style={{ marginTop: "20px" }}
+                      percent={100}
+                      error
+                      active
+                      progress
+                    />
+                  ) : null}
+          </Form>
       <br />
       Zoom:
       <Slider color="red"
         settings={{
           name: 'scale',
           type: 'range',
-          onChange: value => this.handleScale,
-          min: this.state.avatar.allowZoomOut ? parseFloat(0.1) : parseFloat(1) ,
-          max: parseFloat(20),
-          step: parseFloat(1),
-          defaultValue: this.state.avatar.scale,
+          onChange: this.handleScale,
+          min: this.state.allowZoomOut ? parseFloat(1) : parseFloat(2) ,
+          max: 10,
+          step: 1,
+          defaultValue: this.state.scale,
       }} />
       <br />
-      Allow Scale &lt; 1
-      <Input
-        type="checkbox"
+      <Checkbox
+        placeholder='Allow Scale < 1'
+        ref = "allowZoomOut"
+        label = "Allow Scale < 1"
         name="allowZoomOut"
+        defaultChecked = {true}
         onChange= {this.toggleZoomOut}
-        defaultValue="on"
+        toggle
       />
       <br />
-      Border radius:
+      Corner radius:
       <Slider color="red"
         settings={{
           name: 'borderRadius',
           type: 'range',
-          onChange: value => this.handleBorderRadius,
+          onChange: this.handleBorderRadius,
           min: 0,
+          max: 50,
           step: parseFloat(1),
-          defaultValue: this.state.avatar.borderRadius,
+          defaultValue: this.state.borderRadius,
       }} />
-
       <br />
       </div>
     );
