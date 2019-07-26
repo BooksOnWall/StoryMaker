bb406878ac306796417d6aff1df1c0ce79b5bef6
Tom Bouillut <tom@croll.fr>
2019-07-26 13:01:57 -0300

    added sequelize userPrefs model 	nouveau fichier : server/conf/db/userPref.js

---
+++ server/conf/db/userPref.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+module.exports.userPref = {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  value: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  uid: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  }
+};
