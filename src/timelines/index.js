6c2ecfcb340b4d23fbf99d1f5aad74456aa5787b
Tom Bouillut <tom@croll.fr>
2019-08-02 15:58:36 -0300

    adjusting css 	modifié :         src/App.css 	modifié :         src/index.js 	modifié :         src/timelines/index.js

--- src/timelines/index.js
+++ src/timelines/index.js
@@ -13,29 +13,13 @@ const getDefaultTimeline = (node, delay) => {
   return timeline;
 }
 
-const getHomeTimeline = (node, delay) => {
-  const timeline = new Timeline({ paused: true });
-  const texts = node.querySelectorAll('div.view > div');
-
-  timeline
-    .from(node, 0, { display: 'none', autoAlpha: 0, delay })
-    .staggerFrom(texts, 0.375, { autoAlpha: 0, x: -25, ease: Power1.easeOut }, 0.125);
-
-  return timeline;
-}
 
 export const play = (pathname, node, appears) => {
   const delay = appears ? 0 : 0.5;
-  let timeline
-
-  if (pathname === '/')
-    timeline = getHomeTimeline(node, delay);
-  else
-    timeline = getDefaultTimeline(node, delay);
-
+  let timeline = getDefaultTimeline(node, delay);
   window
     .loadPromise
-    .then(() => requestAnimationFrame(() => timeline.play()))
+    .then(() => requestAnimationFrame(() => timeline.play()));
 }
 
 export const exit = (node) => {
