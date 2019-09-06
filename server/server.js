032d05a07736c8650ceb6853ed8f3bfc8d40815f
Tom Bouillut <tom@croll.fr>
2019-09-06 13:05:26 -0300

    try to fix GeoFromText mysql sequelize error 	modifié :         server/server.js

--- server/server.js
+++ server/server.js
@@ -93,6 +93,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define db andD create tables
 const Sequelize = require('sequelize');
+// fix sequelize GeoFromText mac os x bug => https://github.com/sequelize/sequelize/issues/9786
+const wkx = require('wkx')
+Sequelize.GEOMETRY.prototype._stringify = function _stringify(value, options) {
+  return `ST_GeomFromText(${options.escape(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+};
+Sequelize.GEOMETRY.prototype._bindParam = function _bindParam(value, options) {
+  return `ST_GeomFromText(${options.bindParam(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+};
+Sequelize.GEOGRAPHY.prototype._stringify = function _stringify(value, options) {
+  return `ST_GeomFromText(${options.escape(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+};
+Sequelize.GEOGRAPHY.prototype._bindParam = function _bindParam(value, options) {
+  return `ST_GeomFromText(${options.bindParam(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+};
 
 // initialze an instance of Sequelize with mysql conf parameters
 const sequelize = new Sequelize(config.mysql);
