86813b63bfe3070f688c3b98dfe1236d60f1983b
Tom Bouillut <tom@croll.fr>
2019-10-08 11:50:31 -0300

    user avatar moved to vertical bar

--- src/layout/AppMenu.js
+++ src/layout/AppMenu.js
@@ -7,18 +7,22 @@ import {
   Divider,
 } from 'semantic-ui-react';
 import {Link} from 'react-router-dom';
+import UserContext from '../context/UserContext';
 
 import Logo from '../logo.svg';
 
 class AppMenu extends Component {
   constructor(props) {
     super(props);
-
+    let protocol =  process.env.REACT_APP_SERVER_PROTOCOL;
+    let domain = protocol + '://' + process.env.REACT_APP_SERVER_HOST;
+    let server = domain + ':'+ process.env.REACT_APP_SERVER_PORT;
     this.state = {
       locales: this.props.state.locales,
       users: this.props.state.users,
       themes: this.props.state.themes,
       theme: this.props.state.theme,
+      server: server,
       user: this.props.state.user,
       locale: this.props.state.locale,
       authenticated: this.props.childProps.authenticated,
@@ -29,19 +33,26 @@ class AppMenu extends Component {
     return (
       <Menu  className="top" pointing style={{padding: 0}}>
         <Menu.Item name='homepage' active={this.state.activeItem === 'homepage'} as={Link} to='/'  style={{textAlign: 'left'}}>
-        <Image className='App-logo'  fluid  src={Logo} style={{ heigt:'100%'  }} />
+          <Image className='App-logo'  fluid  src={Logo} style={{ heigt:'100%'  }} />
         </Menu.Item>
         {(!this.props.state.sidebarVisible) ?
-        <Menu.Item  className="slide-out">
-        <Button className='hamburger' inverted icon circular
-            onClick={this.props.state.handleSideBar}
-            ><Icon name='bars'  size='big'/></Button>
-        </Menu.Item>
-      : ''}
-        <Divider as={Menu.Item} inverted className='push-donw'  />
-          <Menu.Item name='users' active={this.state.activeItem === 'users'} onClick={this.handleItemClick} as={Link} to='/users' className="userBar"  >
-            <Image  avatar  src={this.state.server + this.props.childProps.user.avatar} />
+          <Menu.Item  className="slide-out">
+            <Button className='hamburger' inverted icon circular
+              onClick={this.props.state.handleSideBar}
+              ><Icon name='bars'  size='big'/></Button>
           </Menu.Item>
+
+      : ''}
+        <Divider as={Menu.Item} inverted className='push-down'  />
+      {this.props.childProps.authenticated ? (
+        <UserContext.Consumer>{(user) => {
+          return(
+            <Menu.Item name='users' active={this.state.activeItem === 'users'} onClick={this.handleItemClick} as={Link} to='/users' className="userBar"  >
+              <Image  avatar  src={this.state.server + this.props.childProps.user.avatar} />
+            </Menu.Item>
+          )}}
+        </UserContext.Consumer>
+      ) : ''}
       </Menu>
     );
   }
--- src/layout/slideMenu.js
+++ src/layout/slideMenu.js
@@ -31,61 +31,50 @@ class LeftSlideMenu extends Component {
       <Segment  fluid='true' style={{ padding: '0'}} >
         <Menu className='bwSidebarMenu' size='huge' primary='true' inverted vertical fluid style={{padding: 0}}>
           {this.props.childProps.authenticated ? (
-            <UserContext.Consumer>{(user) => {
-              return(
-                <Menu.Menu style={{ padding: '0'}}>
-                  <Menu.Item name='dashboard' active={this.state.activeItem === 'dashboard'} onClick={this.handleItemClick} as={Link} to='/dashboard'  >
-                    <Icon size='large' name='dashboard' /><span className='text'>Dashboard</span>
-                  </Menu.Item>
+            <Menu.Menu style={{ padding: '0'}}>
+              <Menu.Item name='dashboard' active={this.state.activeItem === 'dashboard'} onClick={this.handleItemClick} as={Link} to='/dashboard'  >
+                <Icon size='large' name='dashboard' /><span className='text'>Dashboard</span>
+              </Menu.Item>
 
-                  <Divider inverted />
-                  <Menu.Item name='artists' active={this.state.activeItem === 'artists'} onClick={this.handleItemClick} as={Link} to='/artists'  >
-                    <Icon size='large' name='paint brush' /><span className='text'>Artists</span>
-                  </Menu.Item>
-                  <Divider inverted />
-                  <Menu.Item name='stories' active={this.state.activeItem === 'stories'} onClick={this.handleItemClick} as={Link} to='/stories'  >
-                    <Icon size='large' name='book' /><span className='text'>Stories</span>
-                  </Menu.Item>
-                  <Segment inverted >
-                    <Accordion inverted >
-                      <Accordion.Title
-                        active={activeIndex === 0}
-                        index={0}
-                        onClick={this.handleClick}
-                      >
-                        <Icon name='dropdown' />
-                        Languages
-                      </Accordion.Title>
-                      <Accordion.Content className="slide-out" active={activeIndex === 0}>
-                        <Button.Group vertical color='black'>
-                          <Button active ><Flag name='uk' />English</Button>
-                          <Button ><Flag name='es' />Spanish</Button>
-                          <Button ><Flag name='pt' />Portuguese</Button>
-                        </Button.Group>
-                      </Accordion.Content>
-                    </Accordion>
-                  </Segment>
-
-                  <Divider inverted />
-                  <Divider as={Menu.Item} inverted className='push-donw'  />
-
-                  <Menu.Item name='users' active={this.state.activeItem === 'users'} onClick={this.handleItemClick} as={Link} to='/users' className="sidemenu users"  >
-                    <Image  avatar  src={this.state.server + this.props.childProps.user.avatar} /><span className='text'>Users</span>
-                  </Menu.Item>
-                  <Divider inverted />
-                  <Menu.Item name='logout' className='logout' active={this.state.activeItem === 'logout'} onClick={this.handleItemClick} as={Link} to='/logout' >
-                    <Icon name='log out' /><span className='text'>Logout</span>
-                  </Menu.Item>
-                </Menu.Menu>
-            )}}
-          </UserContext.Consumer>
+              <Divider inverted />
+              <Menu.Item name='artists' active={this.state.activeItem === 'artists'} onClick={this.handleItemClick} as={Link} to='/artists'  >
+                <Icon size='large' name='paint brush' /><span className='text'>Artists</span>
+              </Menu.Item>
+              <Divider inverted />
+              <Menu.Item name='stories' active={this.state.activeItem === 'stories'} onClick={this.handleItemClick} as={Link} to='/stories'  >
+                <Icon size='large' name='book' /><span className='text'>Stories</span>
+              </Menu.Item>
+              <Segment inverted >
+                <Accordion inverted >
+                  <Accordion.Title
+                    active={activeIndex === 0}
+                    index={0}
+                    onClick={this.handleClick}
+                    >
+                    <Icon name='dropdown' />
+                    Languages
+                  </Accordion.Title>
+                  <Accordion.Content className="slide-out" active={activeIndex === 0}>
+                    <Button.Group vertical color='black'>
+                      <Button active ><Flag name='uk' />English</Button>
+                      <Button ><Flag name='es' />Spanish</Button>
+                      <Button ><Flag name='pt' />Portuguese</Button>
+                    </Button.Group>
+                  </Accordion.Content>
+                </Accordion>
+              </Segment>
+              <Divider inverted />
+              <Menu.Item name='logout' className='logout' active={this.state.activeItem === 'logout'} onClick={this.handleItemClick} as={Link} to='/logout' >
+                <Icon name='log out' /><span className='text'>Logout</span>
+              </Menu.Item>
+            </Menu.Menu>
           ) : (
             <Menu.Menu >
               <Menu.Item name='homepage' active={this.state.activeItem === 'homepage'} as={Link} to='/'  >
                 <Icon name='home' /><span className='text'>Home</span>
               </Menu.Item>
-            <Menu.Item active={this.state.activeItem === 'login'} onClick={this.handleItemClick} to='/login'  as={Link}><Icon name='connectdevelop' /><span className='text'>Login</span></Menu.Item>
-            <Menu.Item active={this.state.activeItem === 'signup'} onClick={this.handleItemClick} to='/signup' as={Link}><Icon name='connectdevelop' /><span className='text'>SignUp</span></Menu.Item>
+              <Menu.Item active={this.state.activeItem === 'login'} onClick={this.handleItemClick} to='/login'  as={Link}><Icon name='connectdevelop' /><span className='text'>Login</span></Menu.Item>
+              <Menu.Item active={this.state.activeItem === 'signup'} onClick={this.handleItemClick} to='/signup' as={Link}><Icon name='connectdevelop' /><span className='text'>SignUp</span></Menu.Item>
             </Menu.Menu>
           )}
         </Menu>
