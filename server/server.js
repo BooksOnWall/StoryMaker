6a6ab008d5d428ce4186fe2052a90c7facc53665
Tom Bouillut <tom@croll.fr>
2020-01-22 11:09:31 -0300

    commented telegram bot post at mobile zip archive downloaded

--- server/server.js
+++ server/server.js
@@ -1058,7 +1058,7 @@ app.get('/zip/:sid', function(req, res){
     if (err) { throw err; }
     console.log('Story id: '+sid+' Folder size to compress: ',prettyBytes(size));
   });
-  if(hasbot) { bot.telegram.sendMessage(chat_id,"New Story id: "+ sid +" downloaded: Zip size:")}
+  //if(hasbot) { bot.telegram.sendMessage(chat_id,"New Story id: "+ sid +" downloaded: Zip size:")}
 
   res.zip({
     files: [{ path: path + sid, name: sid }],
