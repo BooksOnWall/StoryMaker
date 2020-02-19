9cedb313db5073860f3a5262d63be1f03be0e34b
Tom Bouillut <tom@croll.fr>
2020-02-19 19:55:11 -0300

    Story theme DropZone design

---
+++ src/api/story/theme/bannerPreview.js
@@ -0,0 +1,88 @@
+import React, {useEffect, useState } from 'react';
+import {useDropzone} from 'react-dropzone';
+import {
+  Segment,
+  Image,
+} from 'semantic-ui-react';
+
+const thumbsContainer = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  marginTop: 0
+};
+
+const thumb = {
+  display: 'inline-flex',
+  borderRadius: 2,
+  border: '1px solid #eaeaea',
+  marginBottom: 8,
+  marginRight: 8,
+  width: 210,
+  height: 100,
+  padding: 4,
+  boxSizing: 'border-box'
+};
+
+const thumbInner = {
+  display: 'flex',
+  minWidth: 0,
+  overflow: 'hidden'
+};
+
+const img = {
+  display: 'block',
+  width: 'auto',
+  height: '100%'
+};
+
+
+function BannerPreviews(props) {
+  const [files, setFiles] = useState([]);
+
+  const { getRootProps, getInputProps, isDragActive, isDragReject } = useDropzone({
+
+    accept: 'image/*',
+    minSize: 0,
+    maxSize: 5242880,
+    onDrop: acceptedFiles => {
+      setFiles(acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })));
+      props.state.setBannerImages(acceptedFiles);
+    }
+  });
+
+  const thumbs = files.map(file => (
+    <div style={thumb} key={file.name} >
+      <div style={thumbInner} className='slide-out'>
+        <Image
+          className='fadeIn'
+          alt='preview'
+          src={file.preview}
+          style={img}
+        />
+      </div>
+    </div>
+  ));
+
+  useEffect(() => () => {
+    // Make sure to revoke the data uris to avoid memory leaks
+    files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [files]);
+
+
+  return (
+    <Segment style={{margin:0, padding: 0}} inverted>
+
+      <div {...getRootProps({className: 'dropzone banner'})} style={{ dropzone: {maxHeight: 100 }}}>
+        <input  id='themeBannerFiles' name='files' onChange={props.state.onChangeHandler} ref={ref => this.fileInput = ref} {...getInputProps()} />
+      </div>
+      <aside style={thumbsContainer}>
+        {thumbs}
+      </aside>
+
+    </Segment>
+  );
+}
+export default BannerPreviews;
---
+++ src/api/story/theme/galleryPreview.js
@@ -0,0 +1,89 @@
+import React, {useEffect, useState } from 'react';
+import {useDropzone} from 'react-dropzone';
+import {
+  Segment,
+  Image,
+Divider,
+} from 'semantic-ui-react';
+
+const thumbsContainer = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  marginTop: 16
+};
+
+const thumb = {
+  display: 'inline-flex',
+  borderRadius: 2,
+  border: '1px solid #eaeaea',
+  marginBottom: 8,
+  marginRight: 8,
+  width: 60,
+  height: 60,
+  padding: 4,
+  boxSizing: 'border-box'
+};
+
+const thumbInner = {
+  display: 'flex',
+  minWidth: 0,
+  overflow: 'hidden'
+};
+
+const img = {
+  display: 'block',
+  width: 'auto',
+  height: '100%'
+};
+
+
+function GalleryPreviews(props) {
+  const [files, setFiles] = useState([]);
+
+  const { getRootProps, getInputProps, isDragActive, isDragReject } = useDropzone({
+
+    accept: 'image/*',
+    minSize: 0,
+    maxSize: 5242880,
+    onDrop: acceptedFiles => {
+      setFiles(acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })));
+      props.state.setGalleryImages(acceptedFiles);
+    }
+  });
+
+  const thumbs = files.map(file => (
+    <div style={thumb} key={file.name} >
+      <div style={thumbInner} className='slide-out'>
+        <Image
+          className='fadeIn'
+          alt='preview'
+          src={file.preview}
+          style={img}
+        />
+      </div>
+    </div>
+  ));
+
+  useEffect(() => () => {
+    // Make sure to revoke the data uris to avoid memory leaks
+    files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [files]);
+
+
+  return (
+    <Segment  style={{margin:0, padding: 0}} inverted>
+     <Divider />
+      <div {...getRootProps({className: 'dropzone gallery'})}>
+        <input  id='themeGalleryFiles' name='files' onChange={props.state.onChangeHandler} ref={ref => this.fileInput = ref} {...getInputProps()} />
+      </div>
+      <aside style={thumbsContainer}>
+        {thumbs}
+      </aside>
+    <Divider />
+    </Segment>
+  );
+}
+export default GalleryPreviews;
