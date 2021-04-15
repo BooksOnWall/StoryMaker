978b0e16426e0d174f90caa85dde34a05d176d72
Tom Bouillut <tom@booksonwall.art>
2021-04-15 11:28:01 -0300

    corrected status

--- build.sh
+++ build.sh
@@ -2,11 +2,8 @@
 echo "Checking updates"
 git pull
 yarn
-cd server 
-yarn
-cd ../
 echo "building Books On Wall "
-yarn build 
-echo "adding htaccess en build directory" 
+yarn build
+echo "adding htaccess en build directory"
 cp _htaccess build/.htaccess
 echo "build complete"
