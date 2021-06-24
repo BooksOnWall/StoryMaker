6020749a6c31e4d4a5262298e98412ec8b1373b0
Tom Bouillut <tom@booksonwall.art>
2021-06-24 14:01:04 -0300

    integrating react-three-editable

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,4 +1,31 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { editable as e, configure } from 'react-three-editable';
+// Import your previously exported state
+import editableState from '../utils/default_editableState.json';
+
+const bind = configure({
+  // Enables persistence in development so your edits aren't discarded when you close the browser window
+  enablePersistence: true,
+  // Useful if you use r3e in multiple projects
+  localStorageNamespace: 'StageMaker',
+});
 const HomePage = () => (
-  <div> Hello </div>
+  <Canvas onCreated={bind({ state: editableState })}>
+      <ambientLight intensity={0.5} />
+      {/* Mark objects as editable. */}
+      {/* Properties in the code are used as initial values and reset points in the editor. */}
+      <e.spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        uniqueName="Spotlight"
+      />
+      <e.pointLight uniqueName="PointLight" />
+      <e.mesh uniqueName="Box">
+        <boxBufferGeometry />
+        <meshStandardMaterial color="orange" />
+      </e.mesh>
+    </Canvas>
 )
 export default HomePage;
